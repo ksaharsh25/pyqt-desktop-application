b51816acc2b2812468bd268d8b78914e0fab314f
ksaharsh25 <saharsh@email.com>
2022-12-15 18:48:05 +0530

    loginbackend inprogrss

--- mobile.py
+++ mobile.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication,QMainWindow,QMessageBox
 from PyQt5.uic import loadUi
+# import random
+import MySQLdb as mdb
 
 class login(QMainWindow):
     def __init__(self):
@@ -9,14 +11,30 @@ class login(QMainWindow):
         loadUi("login.ui",self)
         self.loginbutton.clicked.connect(self.loginfunction)
         self.loginbutton.clicked.connect(self.gotoscreen2) 
+        
     def gotoscreen2(self):
         screen2=verify()
         widget.addWidget(screen2)
         widget.setCurrentIndex(widget.currentIndex()+1)    
 
     def loginfunction(self):
-        mobile=self.mobile.text()
-        print("OTP is sent to",mobile) 
+        # mobile=self.mobile.text()
+        # print("OTP is sent to",mobile)
+        con=mdb.connect('localhost','root','','login')
+        with con:
+            cur=con.cursor()
+            cur.execute("INSERT INTO login_db(mobile number)"
+            "VALUES('%s')" % (''.join(self.mobile.text())))
+        
+            # QMessageBox.about(self,"Login output","Congrats party hard!!")
+            self.close()
+        
+
+    # def get_otp():
+    #     otp=""
+        
+    #     otp+= str(random.randint(1,9))
+         
 
 class verify(QMainWindow):
     def __init__(self):
@@ -28,7 +46,7 @@ class verify(QMainWindow):
         OTP=self.otp.text()
         print("Your OTP is",OTP) 
 
-
+    
 
 
 
